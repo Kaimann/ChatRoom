4963bff294bdbd942aab86cec99a79cd4a1f0323
Kaimann <32804771+Kaimann@users.noreply.github.com>
2018-09-13 04:59:22 +0300

    004

--- src/server/ServerTest.java
+++ src/server/ServerTest.java
@@ -4,8 +4,8 @@ import javax.swing.JFrame;
 
 public class ServerTest {
     public static void main(String[] args) {
-        Server iserver = new Server();
-        iserver.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        iserver.startServer();
+        Server myServer = new Server();
+        myServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myServer.startServer();
     }
 }
